6e73c5163d2c26c30c0c1251834570826eac48d5
Ewiase 01 <memaamesaa@gmail.com>
2023-02-23 04:39:05 -0800

    print alpha x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Make alphabet x10 times
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+
+{
+
+	char x;
+
+	int k = 0;
+
+		while (k <= 9)
+	{
+		for (x = 'a'; x <= 'z'; x++)
+		{
+			_putchar(x);
+		}
+
+			_putchar('\n');
+			k++;
+		}
+}
