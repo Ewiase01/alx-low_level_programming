d79f144cd7abd35243432213fcd3464f214877bd
Ewiase 01 <memaamesaa@gmail.com>
2023-02-23 04:14:33 -0800

    11

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Make the alphabet
+ * Return: void
+ */
+
+void print_alphabet(void)
+
+{
+char y;
+for (y = 'a'; y <= 'z'; y++)
+{
+	_putchar(y);
+}
+_putchar('\n');
+}
