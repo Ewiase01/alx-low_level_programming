2975e12e1b157d0325eb50ed355a4eedafce1bc9
Ewiase 01 <memaamesaa@gmail.com>
2023-02-24 08:20:05 -0800

    advance 001b

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,38 +12,28 @@ void print_times_table(int n)
 	{
 		for (i = 0; i <= n; i++)
 		{
-			for (m = 0; m <= n; m++)
-			{ p = i * m;
-				if (m == 0)
-				{
-					_putchar(p + '0');
-				}
-				else if (p < 10 && m != 0)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(p + '0');
-				}
-				else if (p >= 10 && p < 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar((p / 100) + '0');
-					_putchar(((p / 10) % 10) + '0');
-					_putchar((p % 10) + '0');
-				}
-				else if	(p >= 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar((p / 100) + '0');
-					_putchar(((p / 10) % 10) + '0');
-					_putchar((p % 10) + '0');
-				}
-			}
-			_putchar('\n');
+		_putchar('0');
+		for (m = 1; m <= n; m++)
+		{
+		_putchar(',');
+		_putchar(' ');
+		p = i * m;
+		if (p <= 99)
+		_putchar(' ');
+		if (p >= 9)
+		_putchar(' ');
+		if (p >= 100)
+		{
+		_putchar((p / 100) + '0');
+		_putchar(((p / 10)) % 10 + '0');
+		}
+		else if (p <= 99 && p >= 10)
+		{
+		_putchar((p / 10) + '0');
+		}
+		_putchar((p % 10) + '0');
+		}
+		_putchar('\n');
 		}
 	}
 }
