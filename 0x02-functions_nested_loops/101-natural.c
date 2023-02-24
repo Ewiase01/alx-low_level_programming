9cfbc4823af206f4763fb230d176ce811f524b5e
Ewiase 01 <memaamesaa@gmail.com>
2023-02-24 10:19:21 -0800

    advance 002

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - computes and prints the sum of all th multiples
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int x, y = 0;
+
+	while (x < 1024)
+	{
+		if
+			((x % 3 == 0) || (x % 5 == 0))
+			{
+				y += x;
+			}
+		x++;
+	}
+	printf("%d\n"), y);
+	return (0);
+}
