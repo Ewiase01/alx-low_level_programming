fe68bc142c9ac6f4c7ba92f936d8a4173f31c814
Ewiase 01 <memaamesaa@gmail.com>
2023-02-24 11:55:02 -0800

    advance 003b

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - print the first 50 Fibonacci numbers
+ * Return: Always 0.
+ */
+int main(void)
+
+{
+	int a = 0;
+	long int  b = 1;
+	long int c = 2;
+
+	while (a < 50)
+	{
+	if (a == 0)
+	printf("%ld", b);
+	else if (a == 1)
+	printf(", %ld", c);
+	else
+	{
+		c += b;
+		b = c - b;
+		printf(", %ld", c);
+	}
+	++a;
+	}
+	printf("/n");
+	return (0);
+}
