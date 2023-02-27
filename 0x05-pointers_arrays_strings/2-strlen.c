a05eb9261c1b6623eb3be7d966cb246e5dcdb662
Ewiase 01 <memaamesaa@gmail.com>
2023-02-27 14:57:25 -0800

    length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ *
+ * @s: string
+ * Return: Always 0
+ */
+
+int _strlen(char *s)
+{
+	int xcm = 0;
+	
+	while (*s != '\0')
+	{
+		xcm++;
+		s++;
+	}
+
+	return(xcm);
+}
