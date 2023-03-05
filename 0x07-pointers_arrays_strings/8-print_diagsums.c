15d91e433b55da7eb8331fd9cd0009edef7d56e2
Ewiase01 <memaamesaa@gmail.com>
2023-03-05 13:31:51 -0800

    The line of life is a ragged diagonal between duty and desire

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - prints the sum of the two diagonals of a square matrix
+ *
+ * @a: the matrix of the integer
+ * @size: size of  the matrix
+ */
+
+void print_diagsums(int *a, int size)
+
+{
+	int x, y, z;
+
+	x = 0;
+	y = 0;
+
+	for (z = 0; z < size; z++)
+	{
+		x = x + a[z * size + z];
+	}
+	for (z = size - 1; z >= 0; z--)
+	{
+		y += a[z * size + (size - z - 1)];
+	}
+	printf("%d, %d\n", x, y);
+}
