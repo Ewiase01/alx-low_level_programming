ec6e5e3ed06107fa4368edab54b5d1f8d86a5642
Ewiase01 <memaamesaa@gmail.com>
2023-03-20 13:57:24 -0700

    a

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - short description
+ *
+ * @name: first memver
+ * @age: second member
+ * @owner: third member
+ *
+ * Description: longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
