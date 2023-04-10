96c46928d40e4b94f49ab20581284efa13028735
Ewiase01 <memaamesaa@gmail.com>
2023-04-09 19:14:28 -0700

    b003

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,9 +17,9 @@ int create_file(const char *filename, char *text_content)
 	int y = write(x, text_content, strlen(text_content));
 
 	if (filename == NULL)
-
+	{
 		return (-1);
-
+	}
 
 	if (x == -1)
 	{
